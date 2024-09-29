7cbce51c5995ffe06ae1d1727fb11a3ec775fdab
ieb^ <jebwizoscar@hotmail.com>
2024-09-29 19:38:16 +0900

    mkdir -p

--- build_for_github.sh
+++ build_for_github.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 rm outputs/apk/*
 cd android && ./gradlew app:clean app:assembleMultisignRelease && cd ..
+mkdir -p outputs/apk/
 cp -fv android/app/build/outputs/apk/multisign/release/*.apk outputs/apk/
