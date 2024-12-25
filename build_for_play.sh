c0193215c61b978e15206a7b82f1a343de6f437c
ieb^ <jebwizoscar@hotmail.com>
2024-12-25 15:47:46 +0900

    turtle card

--- build_for_play.sh
+++ build_for_play.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-rm outputs/apk/*
 cd android
 
 ./gradlew bundleStoreRelease
@@ -7,8 +6,11 @@ cd android
 
 cd ..
 
+mkdir -p outputs/bundle/
+mkdir -p outputs/bundle-flavor1/
+
 cp -rv android/app/build/outputs/bundle/storeRelease/* outputs/bundle/
-cp -rv android/app/build/outputs/bundle/flavor1Release/* outputs/bundlef1/
+cp -rv android/app/build/outputs/bundle/flavor1Release/* outputs/bundle-flavor1/
 
 for filename in outputs/bundle/*.aab; do
   java -jar signer/apksigner.jar sign --v1-signing-enabled --v2-signing-enabled --v3-signing-enabled=false \
@@ -17,7 +19,7 @@ for filename in outputs/bundle/*.aab; do
   stat -l $filename
 done
 
-for filename in outputs/bundlef1/*.aab; do
+for filename in outputs/bundle-flavor1/*.aab; do
   java -jar signer/apksigner.jar sign --v1-signing-enabled --v2-signing-enabled --v3-signing-enabled=false \
        --ks signer/9esim_key/9eSIMCommunityKey.jks -ks-pass pass:147258369 \
        --min-sdk-version 33 $filename
