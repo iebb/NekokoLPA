48eedc197b6c2d05e5a0951d76ca664a185fb943
ieb^ <jebwizoscar@hotmail.com>
2025-01-04 16:21:35 +0900

    add wenziwanka ckey

--- build_for_play.sh
+++ build_for_play.sh
@@ -7,19 +7,18 @@ cd android
 cd ..
 
 mkdir -p outputs/bundle/
-mkdir -p outputs/bundle-flavor1/
 
 cp -rv android/app/build/outputs/bundle/storeRelease/* outputs/bundle/
-cp -rv android/app/build/outputs/bundle/flavor1Release/* outputs/bundle-flavor1/
+cp -rv android/app/build/outputs/bundle/flavor1Release/* outputs/bundle/
 
-for filename in outputs/bundle/*.aab; do
+for filename in outputs/bundle/app-store*.aab; do
   java -jar signer/apksigner.jar sign --v1-signing-enabled --v2-signing-enabled --v3-signing-enabled=false \
        --ks signer/CommunityKey/CommunityKey.jks -ks-pass pass:CommunityKey \
        --min-sdk-version 33 $filename
   stat -l $filename
 done
 
-for filename in outputs/bundle-flavor1/*.aab; do
+for filename in outputs/bundle/app-flavor1*.aab; do
   java -jar signer/apksigner.jar sign --v1-signing-enabled --v2-signing-enabled --v3-signing-enabled=false \
        --ks signer/9esim_key/9eSIMCommunityKey.jks -ks-pass pass:147258369 \
        --min-sdk-version 33 $filename
