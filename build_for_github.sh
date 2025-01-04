ad2c43627234834ebe7b64868eef581ed18968a7
ieb^ <jebwizoscar@hotmail.com>
2025-01-04 12:50:48 +0900

    add wenziwanka ckey

--- build_for_github.sh
+++ build_for_github.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 rm outputs/apk/*
-cd android && ./gradlew app:clean app:assembleMultisignRelease -PreactNativeArchitectures=arm64-v8a && cd ..
 mkdir -p outputs/apk/
+
+cd android && ./gradlew app:clean app:assembleMultisignRelease -PreactNativeArchitectures=arm64-v8a && cd ..
 cp -fv android/app/build/outputs/apk/multisign/release/*.apk outputs/apk/
 
 cd android && ./gradlew app:clean app:assembleFlavor1Release -PreactNativeArchitectures=arm64-v8a && cd ..
-mkdir -p outputs/apk/
 cp -fv android/app/build/outputs/apk/flavor1/release/*.apk outputs/apk/
