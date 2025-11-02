2cd9299ffd0de795b9ed01bb1e96772657e2c984
ieb^ <jebwizoscar@hotmail.com>
2025-11-03 03:19:05 +0900

    Version 365: release (for new arch)

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,6 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
-const { withSentryConfig } = require("@sentry/react-native/metro");
-
 const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
 
 /**
  * Metro configuration
@@ -18,4 +15,4 @@ const config = {
   },
 };
 
-module.exports = withSentryConfig(mergeConfig(defaultConfig, config));
+module.exports = mergeConfig(defaultConfig, config);
